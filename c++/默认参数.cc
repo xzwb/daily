f8243f56c7e0c86ebf67749d2f6756aa346b322e
xzwb <xzwb@github.com>
2019-10-22 19:42:52 +0800

    c++

---
+++ c++/默认参数.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+std::string make_plural(size_t ctr, const std::string &word, const std::string &ending = "s")
+{
+    return (ctr > 1) ? word + ending : word;
+}
+
+int main()
+{
+    std::cout << make_plural(2, "success", "es") << std::endl;
+    std::cout << make_plural(2, "apple") << std::endl;
+}
